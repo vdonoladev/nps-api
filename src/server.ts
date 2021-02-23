7976cf92296d0c462403bdd0a10fa866fe0fe136
vdonoladev <victordonolaf@gmail.com>
2021-02-23 19:52:48 -0300

    Banco de Dados

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,12 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-/**
+app.use(express.json());
 
-* GET -> Busca
-* POST -> Salvar
-* PUT -> Alterar
-* DELETE -> Deletar
-* PATCH -> Alteração específica
+app.use(router);
 
-*/
-
-app.get("/", (request, response) => {
-	return response.json({message: "Hello World - NLW04"});
-});
-
-// 1 param -> Rota(Recurso API)
-// 2 param -> request, response
-
-app.post("/", (request, response) => {
-	// Recebeu os dados para salvar
-	return response.json({message: "Os dados foram salvos com sucesso!"});
-});
-
-app.listen(3333, () => console.log("Server is running"));
+app.listen(3333, () => console.log("Server is running!"));
