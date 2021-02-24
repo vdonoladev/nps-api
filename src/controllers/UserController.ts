e4b8a977875f3c0c4b44517bdad18ac899794b33
vdonoladev <victordonolaf@gmail.com>
2021-02-24 18:10:43 -0300

    Testando a nossa aplicação

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,32 +1,32 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UserRespository";
 
 class UserController {
-	async create(request: Request, response: Response) {
-		const { name, email } = request.body;
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
 
-		const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UsersRepository);
 
-		const userAlreadyExists = await usersRepository.findOne({
-			email
-		});
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
 
-		if(userAlreadyExists) {
-			return response.status(400).json({
-				error: "User already exists!",
-			});
-		}
+    if (userAlreadyExists) {
+      return response.status(400).json({
+        error: "User already exists!",
+      });
+    }
 
-		const user = usersRepository.create({
-			name,
-			email,
-		});
+    const user = usersRepository.create({
+      name,
+      email,
+    });
 
-		await usersRepository.save(user);
+    await usersRepository.save(user);
 
-		return response.json(user);
-	}
+    return response.status(201).json(user);
+  }
 }
 
 export { UserController };
